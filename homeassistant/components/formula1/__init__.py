bc88ade2d22cc7847b06f8084fbe84c5acf19d3a
Awesomenatorzz@gmail.com <ZpurrreqqFak623>
2023-11-13 16:39:19 +0000

    Add config flow for configuration of the integration and some more initialization

--- homeassistant/components/formula1/__init__.py
+++ homeassistant/components/formula1/__init__.py
@@ -1,6 +1,8 @@
 """The formula1 integration."""
 from __future__ import annotations
 
+import logging
+
 from homeassistant.config_entries import ConfigEntry
 from homeassistant.const import Platform
 from homeassistant.core import HomeAssistant
@@ -12,10 +14,24 @@ from .const import DOMAIN
 PLATFORMS: list[Platform] = [Platform.LIGHT]
 
 
+_LOGGER = logging.getLogger(__name__)
+
+
 async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
     """Set up formula1 from a config entry."""
 
     hass.data.setdefault(DOMAIN, {})
+    if not hass.data[DOMAIN].get("initialized", False):
+        hass.data[DOMAIN].set("initialized", True)
+    else:
+        _LOGGER.warning("Only a single Formula1 integration should be initialized")
+        return False
+
+    _LOGGER.info(entry)
+    # To access the config, it's a simple dict
+    # entry.data["notify_driver_standings_changed"]
+
+    # Could then write to global data as: hass.data[DOMAIN].set/get etc
 
     # Instantiate coordinator
     # Instantiate calendar & event entities
@@ -37,3 +53,8 @@ async def async_unload_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
         hass.data[DOMAIN].pop(entry.entry_id)
 
     return unload_ok
+
+
+async def async_remove_entry(hass: HomeAssistant, _: ConfigEntry) -> None:
+    """Handle removal of an entry."""
+    hass.data[DOMAIN].set("initialized", False)
