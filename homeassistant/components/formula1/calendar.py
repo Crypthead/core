6dca1d35ad3af52b4760a276df3cdfb84c508e87
LukasThimIT <thlukas@student.chalmers.se>
2023-11-15 14:42:23 +0100

    Revert "Skeleton code for the calendar"
    This reverts commit cefff26914725ef01d0d421e297e62dfcca990a0.

--- homeassistant/components/formula1/calendar.py
+++ homeassistant/components/formula1/calendar.py
@@ -1,16 +1,4 @@
-"""Support for F1 Calendar."""
-from datetime import datetime, timedelta
-
-from homeassistant.components.calendar import CalendarEntity, CalendarEvent
-from homeassistant.config_entries import ConfigEntry
-from homeassistant.const import CONF_ID
-from homeassistant.core import HomeAssistant, callback
-from homeassistant.helpers.entity_platform import AddEntitiesCallback
-from homeassistant.helpers.update_coordinator import CoordinatorEntity
-import homeassistant.util.dt as dt_util
-
-from .const import DOMAIN
-from .coordinator import F1Coordinator
+# TODO import everything
 
 # Need to
 # 1. Get data from coordinator
@@ -18,125 +6,19 @@ from .coordinator import F1Coordinator
 # 3. Check that we don't add duplicate events, check if any dissapeared
 
 
-async def async_setup_entry(
-    hass: HomeAssistant,
-    entry: ConfigEntry,
-    async_add_entities: AddEntitiesCallback,
-) -> None:
-    """Set up F1 based on a config entry."""
-    coordinator = hass.data[DOMAIN][entry.data[CONF_ID]]
-    async_add_entities([Formula1Calendar(coordinator, entry)])
+class MyCalendar(CalendarEntity):
 
-
-class Formula1Calendar(CoordinatorEntity[F1Coordinator], CalendarEntity):
-    """Defines a F1 calendar."""
-
-    _attr_has_entity_name = True
-    _attr_icon = "mdi:delete-empty"
-    _attr_name = None
-
-    def __init__(self, coordinator: F1Coordinator, entry: ConfigEntry) -> None:
+    def __init__(self, coordinator):
         """Pass coordinator to CoordinatorEntity."""
-        super().__init__(coordinator, entry)
-        self._event: CalendarEvent | None = None
-
-    @property
-    def event(self) -> CalendarEvent | None:
-        """Return the next upcoming event."""
-        return self._event
-
-    async def async_get_events(
-        self, hass: HomeAssistant, start_date: datetime, end_date: datetime
-    ) -> list[CalendarEvent]:
-        """Return calendar events within a datetime range."""
-        events: list[CalendarEvent] = []
-
-        schedule = self.coordinator.data.items()
-        # GET ALL EVENTS BETWEEN START AND END DATE
-        for _, race in schedule.iterrows():
-            session_dates = race[
-                [
-                    "Session1DateUtc",
-                    "Session2DateUtc",
-                    "Session3DateUtc",
-                    "Session4DateUtc",
-                    "Session5DateUtc",
-                ]
-            ]
-
-            for i, session_date in enumerate(session_dates.values):
-                if (
-                    session_date >= start_date
-                    and session_date + timedelta(hours=2) < end_date
-                ):
-                    event_summary = (
-                        str(race["RoundNumber"])
-                        + " "
-                        + str(race["Country"])
-                        + " "
-                        + str(race["EventName"])
-                        + " "
-                        + str(race["Session" + str(i + 1)])
-                    )
-
-                    events.append(
-                        CalendarEvent(
-                            summary=event_summary,
-                            start=session_date,
-                            end=session_date + timedelta(hours=2),
-                        )
-                    )
-
-        return events
+        super().__init__(coordinator, context=idx)
+        self.idx = idx
 
     @callback
     def _handle_coordinator_update(self) -> None:
         """Handle updated data from the coordinator."""
-        schedule = self.coordinator.data.items()
+        self._attr_is_on = self.coordinator.data[self.idx]["state"]
+        self.async_write_ha_state()
 
-        # FIND NEXT UPCOMING RACE
-        event_start = None
-        event_summary = None
-
-        for _, race in schedule.iterrows():
-            session_dates = race[
-                [
-                    "Session1DateUtc",
-                    "Session2DateUtc",
-                    "Session3DateUtc",
-                    "Session4DateUtc",
-                    "Session5DateUtc",
-                ]
-            ]
-
-            for i, session_date in enumerate(session_dates.values):
-                if session_date >= dt_util.now().date() and (
-                    session_date < event_start or event_start is None
-                ):
-                    event_start = session_date
-
-                    event_summary = (
-                        str(race["RoundNumber"])
-                        + " "
-                        + str(race["Country"])
-                        + " "
-                        + str(race["EventName"])
-                        + " "
-                        + str(race["Session" + str(i + 1)])
-                    )
-
-                    break
-
-        if event_start is not None and event_summary is not None:
-            self._event = CalendarEvent(
-                summary=event_summary,
-                start=event_start,
-                end=event_start + timedelta(hours=2),
-            )
-
-        super()._handle_coordinator_update()
-
-    async def async_added_to_hass(self) -> None:
-        """When entity is added to hass."""
-        await super().async_added_to_hass()
-        self._handle_coordinator_update()
+    async def async_create_event(self, **kwargs: Any) -> None:
+        """Add a new event to calendar."""
+        event = CalendarEvent()
