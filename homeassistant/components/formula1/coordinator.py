9768af5c44e8b04d9ba1e888d08ffa39da238ae5
koutsakis.d <32978066+koutsd@users.noreply.github.com>
2023-11-16 14:39:44 +0000

    fixed linter errors

--- homeassistant/components/formula1/coordinator.py
+++ homeassistant/components/formula1/coordinator.py
@@ -1,28 +1,19 @@
 """Example integration using DataUpdateCoordinator."""
-
-from datetime import timedelta
+# pylint: disable=broad-except
+from datetime import date, timedelta
 import logging
 
-import async_timeout
-
-from homeassistant.components.light import LightEntity
-from homeassistant.core import callback
-from homeassistant.exceptions import ConfigEntryAuthFailed
-from homeassistant.helpers.update_coordinator import (
-    CoordinatorEntity,
-    DataUpdateCoordinator,
-    UpdateFailed,
-)
-
 import fastf1
+
+from homeassistant.helpers.update_coordinator import DataUpdateCoordinator
 
 _LOGGER = logging.getLogger(__name__)
 
 
-class MyCoordinator(DataUpdateCoordinator):
+class F1Coordinator(DataUpdateCoordinator):
     """My custom coordinator."""
 
-    def __init__(self, hass, my_api):
+    def __init__(self, hass):
         """Initialize my coordinator."""
         super().__init__(
             hass,
@@ -30,9 +21,8 @@ class MyCoordinator(DataUpdateCoordinator):
             # Name of the data. For logging purposes.
             name="Fast-F1 API",
             # Polling interval. Will only be polled if there are subscribers.
-            update_interval=timedelta(seconds=30),
+            update_interval=timedelta(hours=12),
         )
-        self.my_api = fastf1
 
     async def _async_update_data(self):
         """Fetch data from API endpoint.
@@ -41,10 +31,27 @@ class MyCoordinator(DataUpdateCoordinator):
         so entities can quickly look up their data.
         """
         try:
-            data = fastf1.get_data_somehow()
-            # TODO get info and parse so others can use it
-            # Find out whether they must be returned from the method or saved somehow
-        except Exception as e:
+            # Get current year's schedule
+            data = fastf1.get_event_schedule(
+                date.today().year,
+                include_testing=False,
+                backend="fastf1",
+                force_ergast=False,
+            )
+
+            data.drop(
+                [
+                    "Session1Date",
+                    "Session2Date",
+                    "Session3Date",
+                    "Session4Date",
+                    "Session5Date",
+                ],
+                axis=1,
+                inplace=True,
+            )
+
+            return data
+
+        except Exception:
             pass
-
-
