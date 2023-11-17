a2df19e944ab19d3a2e94e87a1296b5d85c2b686
RedSpettan <31245545+RedSpettan@users.noreply.github.com>
2023-11-17 17:01:58 +0000

    :hammer: Fixed long method code smell by method extraction

--- homeassistant/components/formula1/coordinator.py
+++ homeassistant/components/formula1/coordinator.py
@@ -1,13 +1,13 @@
 """Example integration using DataUpdateCoordinator."""
 # pylint: disable=broad-exception-caught
-from datetime import date, timedelta
-import homeassistant.util.dt as dt_util
+from datetime import timedelta
 import logging
 
 import fastf1
 from fastf1.ergast import Ergast
 
 from homeassistant.helpers.update_coordinator import DataUpdateCoordinator
+import homeassistant.util.dt as dt_util
 
 _LOGGER = logging.getLogger(__name__)
 
@@ -39,70 +39,11 @@ class F1Coordinator(DataUpdateCoordinator):
 
         """
         try:
-            # Get current year's schedule (RoundNumber, Country, Location, Session1, Session1Date,
-            #  Session2, Session2Date,  Session3, Session3Date,  Session4, Session4Date,  Session5, Session5Date)
-            schedule = await self.hass.async_add_executor_job(
-                fastf1.get_event_schedule, date.today().year
-            )
+            schedule = await self._get_schedule()
 
-            schedule.drop(
-                [
-                    "Location",
-                    "OfficialEventName",
-                    "EventDate",
-                    "EventFormat",
-                    "Session1DateUtc",
-                    "Session2DateUtc",
-                    "Session3DateUtc",
-                    "Session4DateUtc",
-                    "Session5DateUtc",
-                    "F1ApiSupport",
-                ],
-                axis=1,
-                inplace=True,
-            )
+            driver_standings = await self._get_driver_standings()
 
-            schedule.drop(schedule[schedule["Session5"] == "None"].index, inplace=True)
-
-            # Driver Standings (position, points, givenName, familyName, constructorNames)
-
-            driver_standings = await self.hass.async_add_executor_job(
-                self.ergast.get_driver_standings, dt_util.now().today().year
-            )
-
-            driver_standings = driver_standings.content[0].drop(
-                [
-                    "positionText",
-                    "wins",
-                    "driverId",
-                    "driverNumber",
-                    "driverCode",
-                    "driverUrl",
-                    "dateOfBirth",
-                    "driverNationality",
-                    "constructorIds",
-                    "constructorUrls",
-                    "constructorNationalities",
-                ],
-                axis=1,
-            )
-
-            # Constructor Standings (position, points, constructorName)
-
-            constructor_standings = await self.hass.async_add_executor_job(
-                self.ergast.get_constructor_standings, date.today().year
-            )
-
-            constructor_standings = constructor_standings.content[0].drop(
-                [
-                    "positionText",
-                    "wins",
-                    "constructorId",
-                    "constructorUrl",
-                    "constructorNationality",
-                ],
-                axis=1,
-            )
+            constructor_standings = await self._get_constructor_standings()
 
             data_dict = {
                 "schedule": schedule,
@@ -116,3 +57,98 @@ class F1Coordinator(DataUpdateCoordinator):
         except Exception as e:
             _LOGGER.error("Error fetching F1 data: %s", e)
             raise
+
+    async def _get_constructor_standings(self):
+        """Get current constructor standings.
+
+        Returns
+        -------
+        DataFrame with columns:
+
+        position, points, constructorName
+        """
+
+        constructor_standings = await self.hass.async_add_executor_job(
+            self.ergast.get_constructor_standings, dt_util.now().today().year
+        )
+
+        constructor_standings = constructor_standings.content[0].drop(
+            [
+                "positionText",
+                "wins",
+                "constructorId",
+                "constructorUrl",
+                "constructorNationality",
+            ],
+            axis=1,
+        )
+
+        return constructor_standings
+
+    async def _get_driver_standings(self):
+        """Get current driver standings.
+
+        Returns
+        -------
+        DataFrame with columns:
+
+        position, points, givenName, familyName, constructorNames
+        """
+
+        driver_standings = await self.hass.async_add_executor_job(
+            self.ergast.get_driver_standings, dt_util.now().today().year
+        )
+
+        driver_standings = driver_standings.content[0].drop(
+            [
+                "positionText",
+                "wins",
+                "driverId",
+                "driverNumber",
+                "driverCode",
+                "driverUrl",
+                "dateOfBirth",
+                "driverNationality",
+                "constructorIds",
+                "constructorUrls",
+                "constructorNationalities",
+            ],
+            axis=1,
+        )
+
+        return driver_standings
+
+    async def _get_schedule(self):
+        """Get current year's schedule.
+
+        Returns
+        -------
+        DataFrame with columns:
+
+        RoundNumber, Country, Location, Session1, Session1Date,
+        Session2, Session2Date,  Session3, Session3Date,  Session4, Session4Date,  Session5, Session5Date
+        """
+
+        schedule = await self.hass.async_add_executor_job(
+            fastf1.get_event_schedule, dt_util.now().today().year
+        )
+
+        schedule.drop(
+            [
+                "Location",
+                "OfficialEventName",
+                "EventDate",
+                "EventFormat",
+                "Session1DateUtc",
+                "Session2DateUtc",
+                "Session3DateUtc",
+                "Session4DateUtc",
+                "Session5DateUtc",
+                "F1ApiSupport",
+            ],
+            axis=1,
+            inplace=True,
+        )
+
+        schedule.drop(schedule[schedule["Session5"] == "None"].index, inplace=True)
+        return schedule
