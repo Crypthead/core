deb8e326ca13f55946692b8ec175a82df96b6a92
RedSpettan <31245545+RedSpettan@users.noreply.github.com>
2023-11-28 09:38:00 +0000

    :sparkles: :hammer: Updated coordinator tests
    Co-authored-by: Henrik <54803889+henkejson@users.noreply.github.com>

--- tests/components/formula1/test_coordinator.py
+++ tests/components/formula1/test_coordinator.py
@@ -1,28 +1,54 @@
-from unittest.mock import patch, AsyncMock
-import pytest
-import pandas as pd
-import sys
+"""Tests for F1 Coordinator."""
+
 import os
-sys.path.append(os.path.dirname(os.path.dirname(os.path.dirname(os.path.dirname(__file__)))))
+import sys
+from unittest.mock import AsyncMock, patch
+
+import pytest
 
 from homeassistant.components.formula1.coordinator import F1Coordinator
+from homeassistant.core import HomeAssistant
 
-import asyncio
+sys.path.append(
+    os.path.dirname(os.path.dirname(os.path.dirname(os.path.dirname(__file__))))
+)
 
 
+@pytest.mark.asyncio
+async def test_async_update_data(hass: HomeAssistant):
+    """Test for fetching data."""
+    # Mocking the external dependencies
+    with patch("homeassistant.components.formula1.coordinator.Ergast") as mock_ergast:
+        mock_ergast_instance = mock_ergast.return_value
+        mock_ergast_instance._get_schedule = AsyncMock(return_value="mock_schedule")
+        mock_ergast_instance._get_driver_standings = AsyncMock(
+            return_value="mock_driver_standings"
+        )
+        mock_ergast_instance._get_constructor_standings = AsyncMock(
+            return_value="mock_constructor_standings"
+        )
+        mock_ergast_instance._get_last_race_results = AsyncMock(
+            return_value="mock_last_race_results"
+        )
+        mock_ergast_instance._get_last_race_info = AsyncMock(
+            return_value="mock_last_race_info"
+        )
 
-async def test_async_update_data_success():
-    mock_hass = AsyncMock()
-    coordinator = F1Coordinator(mock_hass)
-    await coordinator.async_config_entry_first_refresh()
+        # Mocking the logger
+        with patch(
+            "homeassistant.components.formula1.coordinator._LOGGER"
+        ) as mock_logger:
+            coordinator = F1Coordinator(hass)  # Mock the hass object as needed
+            data = await coordinator._async_update_data()
 
-    data = await coordinator.async_request_refresh()
+            # Verify the data
+            assert data == {
+                "schedule": "mock_schedule",
+                "driver_standings": "mock_driver_standings",
+                "constructor_standings": "mock_constructor_standings",
+                "last_race_results": "mock_last_race_results",
+                "last_race_info": "mock_last_race_info",
+            }
 
-    print(data)
-
-async def main():
-    await test_async_update_data_success()
-
-if __name__ == "__main__":
-    loop = asyncio.get_event_loop()
-    loop.run_until_complete(main())
+            # Verify if the log message was called
+            mock_logger.info.assert_called_once()
