de51a818e13a5dbd93c3f784232e78f77dfdaa65
RedSpettan <31245545+RedSpettan@users.noreply.github.com>
2023-12-01 15:30:16 +0000

    :hammer: Created an upgraded Coordinator Fixture
    Co-authored-by: Henrik <54803889+henkejson@users.noreply.github.com>

--- tests/components/formula1/conftest.py
+++ tests/components/formula1/conftest.py
@@ -8,6 +8,7 @@ from pandas import Timestamp
 import pytest
 
 from homeassistant.components.formula1.const import DOMAIN
+from homeassistant.components.formula1.coordinator import F1Coordinator
 from homeassistant.config_entries import SOURCE_USER
 import homeassistant.util.dt as dt_util
 
@@ -37,6 +38,45 @@ def config_entry_fixture():
 
 
 MOCK_DATETIME = dt_util.as_utc(datetime(2017, 11, 27, 0, 0, 0))
+
+
+@pytest.fixture(name="f1_coordinator")
+async def f1_coordinator(hass):
+    """Mock F1 Coordinator."""
+    # Mock the API methods
+    with patch.object(
+        F1Coordinator, "_get_schedule", new_callable=AsyncMock
+    ) as mock_schedule:
+        # Set return values for the mock methods
+        mock_schedule.return_value = pd.DataFrame(
+            {
+                "EventName": ["Test Event"],
+                "Session1Date": [Timestamp(MOCK_DATETIME + timedelta(days=1))],
+                "Session2Date": [Timestamp(MOCK_DATETIME + timedelta(days=2))],
+                "Session3Date": [Timestamp(MOCK_DATETIME + timedelta(days=3))],
+                "Session4Date": [Timestamp(MOCK_DATETIME + timedelta(days=3, hours=5))],
+                "Session5Date": [Timestamp(MOCK_DATETIME + timedelta(days=5))],
+                "Session1": ["Session1"],
+                "Session2": ["Session2"],
+                "Session3": ["Session3"],
+                "Session4": ["Session4"],
+                "Session5": ["Session5"],
+                "RoundNumber": ["1"],
+                "Country": ["Test Country"],
+            }
+        )
+        # mock_driver_standings.return_value = ...
+        # mock_constructor_standings.return_value = ...
+        # mock_last_race_results.return_value = ...
+        # mock_last_race_info.return_value = ...
+
+        # Initialize the coordinator
+        coordinator = F1Coordinator(hass)
+
+        # Use async_config_entry_first_refresh for initial data retrieval
+        await coordinator.async_config_entry_first_refresh()
+
+        yield coordinator
 
 
 @pytest.fixture(name="coordinator")
--- tests/components/formula1/test_calendar.py
+++ tests/components/formula1/test_calendar.py
@@ -7,15 +7,15 @@ from homeassistant.components.formula1.calendar import Formula1Calendar
 from homeassistant.core import HomeAssistant
 import homeassistant.util.dt as dt_util
 
-pytestmark = pytest.mark.usefixtures("config_entry", "coordinator")
+pytestmark = pytest.mark.usefixtures("config_entry", "coordinator", "f1_coordinator")
 
 
 MOCK_DATETIME = dt_util.as_utc(datetime(2017, 11, 27, 0, 0, 0))
 
 
-def test_create_calendar_event(hass: HomeAssistant, config_entry, coordinator):
+def test_create_calendar_event(hass: HomeAssistant, config_entry, f1_coordinator):
     """Test creating a calendar event."""
-    calendar = Formula1Calendar(coordinator, config_entry)
+    calendar = Formula1Calendar(f1_coordinator, config_entry)
 
     event = calendar._create_calendar_event(
         "Test Event", "Session1", "1", MOCK_DATETIME, "Test Country"
@@ -28,9 +28,9 @@ def test_create_calendar_event(hass: HomeAssistant, config_entry, coordinator):
 
 
 @pytest.mark.asyncio
-async def test_async_get_events_race(hass: HomeAssistant, config_entry, coordinator):
+async def test_async_get_events_race(hass: HomeAssistant, config_entry, f1_coordinator):
     """Test async_get_events with only race events."""
-    calendar = Formula1Calendar(coordinator, config_entry)
+    calendar = Formula1Calendar(f1_coordinator, config_entry)
 
     events = await calendar.async_get_events(
         hass, MOCK_DATETIME, MOCK_DATETIME + timedelta(days=10)
@@ -40,10 +40,10 @@ async def test_async_get_events_race(hass: HomeAssistant, config_entry, coordina
 
 @pytest.mark.asyncio
 async def test_async_get_events_no_events(
-    hass: HomeAssistant, config_entry, coordinator
+    hass: HomeAssistant, config_entry, f1_coordinator
 ):
     """Test async_get_events with no events."""
-    calendar = Formula1Calendar(coordinator, config_entry)
+    calendar = Formula1Calendar(f1_coordinator, config_entry)
 
     events = await calendar.async_get_events(
         hass, MOCK_DATETIME + timedelta(days=1), MOCK_DATETIME + timedelta(days=2)
@@ -53,10 +53,10 @@ async def test_async_get_events_no_events(
 
 @pytest.mark.asyncio
 async def test_async_get_events_all_events(
-    hass: HomeAssistant, config_entry, coordinator
+    hass: HomeAssistant, config_entry, f1_coordinator
 ):
     """Test async_get_events with all event types."""
-    calendar = Formula1Calendar(coordinator, config_entry)
+    calendar = Formula1Calendar(f1_coordinator, config_entry)
     calendar.only_show_race_event = False
 
     events = await calendar.async_get_events(
