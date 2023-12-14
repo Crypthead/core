02316a169eb754497336fd7877201218ae525d8d
Henrik <henke.asaka@gmail.com>
2023-12-14 15:44:33 +0100

    Improve faking dates for weather

--- homeassistant/components/formula1/coordinator.py
+++ homeassistant/components/formula1/coordinator.py
@@ -6,6 +6,7 @@ import logging
 import fastf1
 from fastf1.ergast import Ergast
 import python_weather
+import datetime as dt
 
 from homeassistant.helpers.update_coordinator import DataUpdateCoordinator
 import homeassistant.util.dt as dt_util
@@ -249,7 +250,7 @@ class F1Coordinator(DataUpdateCoordinator):
             session_date = (
                 next_event[["Session" + str(i) + "Date"]].item()
                 if not fake_dates
-                else dt_util.now().today()
+                else (dt_util.now().today() + dt.timedelta(days=(i-1)//2))
             )
 
             for forecast in weather.forecasts:
